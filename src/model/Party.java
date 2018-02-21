d3c0bc57938b88562da87fe39c7a7b6ba8249537
myriamvanerum <myriamvanerum@hotmail.com>
2018-02-21 15:50:13 +0100

    Added attributes

--- src/model/Party.java
+++ src/model/Party.java
@@ -1,5 +1,9 @@
 package model;
 
-public class Party {
+import java.awt.geom.Point2D;
 
+public class Party {
+	String instanceName, className;
+	Integer posInSeqDiagram;
+	Point2D posInCommDiagram;
 }
