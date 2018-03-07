83bfd7d90077a50eb2cbdb27cb023e76b6aeb405
Caroke <caroline.buntinx.rpmd@gmail.com>
2018-03-07 22:35:16 +0100

    MessageTest en PartyTest JUnit

--- src/model/Actor.java
+++ src/model/Actor.java
@@ -1,9 +1,11 @@
 package model;
 
-/*
+/**
  * An Actor class. An actor is a kind of party
  * 
- * @author SWOP groep 03
+ * @author groep 03
+ * 
+ *
  */
 public class Actor extends Party {
 	/**
--- src/model/Object.java
+++ src/model/Object.java
@@ -1,9 +1,9 @@
 package model;
 
-/*
+/**
  * An Object class
  * 
- * @author SWOP groep 03
+ * @author groep 03
  */
 public class Object extends Party {
 
