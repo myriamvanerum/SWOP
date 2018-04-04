60e2a31045dfa1d56552e2a4adf415aafd42475a
julie <julie.haesendonckx@student.kuleuven.be>
2018-04-04 20:38:35 +0200

    aanpassingen view tonen van party met label

--- src/view/ViewLabel.java
+++ src/view/ViewLabel.java
@@ -3,26 +3,27 @@ package view;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+import com.sun.javafx.geom.Point2D;
+
 public class ViewLabel {
 	/**
 	 * This method draws a label for the components
 	 * @param g
 	 * 		The graphics library used
 	 * @param label
-	 * 		The label to draw
-	 * @param color
+	 * 		The text of the label
+	 * @param position
 	 * 		The color to draw the label in
 	 * @throws IllegalArgumentException
 	 * 		Illegal label or color
 	 */
-	private void draw(Graphics2D g, /*Label label, */Color color) {
-//		if (label == null || color == null)
-//			throw new IllegalArgumentException();
-//		if (label.getText() != null)
-//			label.setWidth(g);
-//		
-//		g.setColor(color);
-//		g.drawString(label.getText(), label.getX(), label.getY());
-//		g.setColor(new Color(0,0,0));
+	protected void draw(Graphics2D g, String label, Point2D position) {
+		if (label == null || position.x < 0 || position.y < 0)
+			throw new IllegalArgumentException();
+
+		// TODO check label state
+		g.setColor(new Color(0,0,0));
+		g.drawString(label, position.x, position.y);
+		g.setColor(new Color(0,0,0));
 	}
 }
