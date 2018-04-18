9f3be8ed476c7ecfb2881f6135c9125bdd360257
myriamvanerum <myriamvanerum@hotmail.com>
2018-04-18 17:27:31 +0200

    bezig met recordings. Main methode verhuisd

---
+++ test/test/RecordingTool.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.io.File;
+import java.util.Scanner;
+
+import view.MainWindow;
+
+public class RecordingTool {
+	private static String name;
+	private static Scanner scanner = new Scanner(System.in);
+	
+	private static String directory = System.getProperty("user.dir") + File.separator + "testing" + File.separator + "recordings";
+	
+	public static void main(String[] args) {
+		make(directory);
+		
+		System.out.println("Recording name:");
+		
+		name = scanner.nextLine();
+		String file = directory + File.separator + name;
+		
+		Boolean exists = make(file);
+		if (exists)
+			return;
+		
+		java.awt.EventQueue.invokeLater(() -> {
+			MainWindow main = new MainWindow(file);
+			main.recordSession(directory + File.separator + "recording");
+			main.show();
+		});
+	}
+	
+	private static boolean make(String dir) {
+		File f = new File(dir);
+		if (!f.exists()) {
+			f.mkdir();
+			return false;
+		}
+		return true;
+	}
+}
