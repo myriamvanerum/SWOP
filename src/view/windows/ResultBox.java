cf7b4b72d6cf54277713ab6c0c8104d0e6a83249
julie <julie.haesendonckx@student.kuleuven.be>
2018-05-13 11:55:33 +0200

    tonen result box werkt

--- src/view/windows/ResultBox.java
+++ src/view/windows/ResultBox.java
@@ -12,11 +12,11 @@ public class ResultBox extends DialogBox {
 	private ViewLabel viewLabel;
 		
 	public ResultBox(Message message, int x, int y) {
-		super(x, y, 250, 50, new Titlebar(x,y,250));
+		super(x, y, 250, 60, new Titlebar(x,y,250));
 		this.message = message;
 		this.viewLabel = new ViewLabel(message.getLabel());
 		this.controls = new ArrayList<WindowControl>();
-		controls.add(new TextBox("Label: ", getX()+10, getY()+20, viewLabel));
+		controls.add(new TextBox("Label: ", getX()+10, getY()+40, viewLabel));
 		this.currentControl = 0;
 		setTitle("Result message");
 	}
